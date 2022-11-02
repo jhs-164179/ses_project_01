0636ef8b508057a9d528e9f1a1708815e075e594
jhs <ggyo003@naver.com>
2022-11-02 13:50:35 +0900

    hi plz

--- project1/project1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from accounts.views import home
-
-urlpatterns = [
-    path('', home),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-]
