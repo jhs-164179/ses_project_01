92a8555628fe95785cf32ac7eb553b81aa870804
jhs <ggyo003@naver.com>
2022-11-09 09:13:31 +0900

    jhsjhs

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import register, login, logout
+
+app_name = "accounts"
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
