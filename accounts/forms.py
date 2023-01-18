6df651467a9f686463ebcb569c095bcd70920695
hyeonseok jin <ggyo003@naver.com>
2023-01-18 14:33:09 +0900

    yongg

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,7 +8,7 @@ from django.contrib.auth.hashers import check_password
 class LoginForm(forms.Form):
     useremail = forms.EmailField(
         error_messages={
-            'required': '이메일주소를 입력해주세요.'
+            'required': '아이디를 입력해주세요.'
         }, max_length=64, label="Email")
     password = forms.CharField(
         error_messages={
@@ -22,16 +22,18 @@ class LoginForm(forms.Form):
         password = cleaned_data.get('password')
 
         if useremail and password:
-            # user = User.objects.get(user_email=useremail)
-            user = User.objects.filter(user_email=useremail)
-            # print(len(user)) # ID 존재여부를 user 객체의 길이로 판별한다.
-            # print(user.values('user_pw')[0]['user_pw']) # 암호화된 비밀번호 추출
-            if len(user) != 1 :
-                self.add_error('useremail', '존재하지 않는 이메일주소를 입력하였습니다.')
-            elif not check_password(password, user.values('user_pw')[0]['user_pw']): #user.user_pw
-                self.add_error('password', '비밀번호가 일치하지 않습니다.')
+            try:
+                user = User.objects.get(user_email = useremail)
+            except User.DoesNotExist:
+                self.add_error("useremail", "아이디가 존재하지 않습니다.")
+                return
+
+            if not check_password(password, user.user_pw):
+                self.add_error("password", "비밀번호가 일치하지 않습니다.")
             else:
-                self.user_name = user.values('user_name')[0]['user_name'] #user.user_name
+                self.user_name = user.user_name
+ 
+        
         
         
 
