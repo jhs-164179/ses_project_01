c1335964c487dde1f8b9e9a7d7d8c443743054b6
SeongJun Hong <tjdwns2759@gmail.com>
2023-01-19 18:21:13 +0900

    hong21195

--- pr_01/urls.py
+++ pr_01/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('board/', include('board.urls')),
     path('data_pg/', include('datapg.urls')),
-    path('', include('datapg.urls')),
+    path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
     path('user/', include('user.urls')),
     re_path('accounts/', include('allauth.urls'))
